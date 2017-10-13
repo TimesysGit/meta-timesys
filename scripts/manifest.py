ab82bae57b7216d2f170adf14701e8adf4f306cb
Steve Bedford <steve.bedford@timesys.com>
2017-10-13 15:37:59 -0400

    Rewrite scripts/manifest.py to use Tinfoil
    We had some issues with the custom BBCooker subclass and our own runner
    that I didn't observe with Tinfoil. This commit is a pretty significant
    rewrite to use Tinfoil instead, plus some general cleanup.
    
    No longer struggling with AUTOREV, different distros, or multiple git
    remotes in layers.

--- scripts/manifest.py
+++ scripts/manifest.py
@@ -1,63 +1,116 @@
 #!/usr/bin/env python3
+# Copyright (C) 2017 Timesys Corporation
 
+from datetime import datetime
+import logging
 import json
 import os
 import re
 import sys
-import subprocess
-import logging
 
-if len(sys.argv) != 4:
-    usage()
-
-def usage():
-    print("This script generates a json manifest file of the recipes built for an image.")
-    print("manifest.py <bitbake-root> <bitbake-target> <output-file>")
-    sys.exit(1)
-
+# expects bitbake dir, target image, output filename
 broot = sys.argv[1]
 target = sys.argv[2]
 ofile = sys.argv[3]
 
-# need to add our lib folder, so first figure out where manifest.py lives
+# add bitbake's lib and our layer's lib folders to import path
 bindir = os.path.dirname(__file__)
-# get parent dir
 topdir = os.path.dirname(bindir)
-# add libs to path for imports
 sys.path[0:0] = [os.path.join(broot, 'lib'), os.path.join(topdir, 'lib')]
 
-from cooker import TimesysCooker
+import bb
+import bb.tinfoil
+from backport import generatePkgDepTreeData
 from utils import get_file_layer, get_layer_info
 
 logger = logging.getLogger('BitBake')
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.INFO)
 
-cooker = TimesysCooker()
-manifest = dict()
-logger.critical ("trying to start cooker")
-if cooker.start():
-    logger.info ("started cooker")
 
-    # get a list of all the recipes and providing files (layers)
-    (latest_versions, preferred_versions) = bb.providers.findProviders(cooker.data, cooker.recipecaches[''], cooker.recipecaches[''].pkg_pn)
-    allproviders = bb.providers.allProviders(cooker.recipecaches[''])
+def setup_tinfoil(tracking=False):
+    tinfoil = bb.tinfoil.Tinfoil(tracking=tracking)
+    tinfoil.logger.setLevel(logger.getEffectiveLevel())
 
-    logger.info ("running for %s" % target)
-    # generate dependencies for the image target passed, image_complete is last image task
-    depgraph = cooker.generatePkgDepTreeData([target], 'build')
+    options = bb.tinfoil.TinfoilConfigParameters(parse_only=True, dry_run=True)
+    tinfoil.config.setConfigParameters(options)
+    tinfoil.cooker.featureset.setFeature(bb.cooker.CookerFeatures.HOB_EXTRA_CACHES)
 
-    # get all the layers currently used
-    layer_info = { lyr['name'] : dict(collection=lyr.get('collection', 'UNKNOWN'), remote=lyr['remote'], rev=lyr['revision'], branch=lyr['branch']) for lyr in get_layer_info(cooker) }
-    manifest = dict(layers=layer_info, packages=dict(), image=target, distro=cooker.data.get('DISTRO_CODENAME'), distro_version=cooker.data.get('DISTRO_VERSION'), machine=cooker.data.get('MACHINE'))
-    preffiles = []
-    items = dict() # pkg -> { version, layers : [{ info }] }
+    tinfoil.bblayers = (tinfoil.config_data.getVar('BBLAYERS', True) or "").split()
+    layerconfs = tinfoil.config_data.varhistory.get_variable_items_files(
+        'BBFILE_COLLECTIONS', tinfoil.config_data)
+    tinfoil.cooker.bbfile_collections = {
+        layer: os.path.dirname(os.path.dirname(path))
+        for layer, path in layerconfs.items()}
 
+    # on this instance, use backported generatePkgDepTreeData()
+    tinfoil.cooker.generatePkgDepTreeData = \
+        generatePkgDepTreeData.__get__(tinfoil.cooker, tinfoil.cooker.__class__)
+
+    tinfoil.prepare()
+    return tinfoil
+
+
+def find_patched_cves(cooker_data, realfn):
+    # iterate over the recipes which would be built (pn-buildlist)
     cve_match = re.compile("CVE:( CVE\-\d{4}\-\d+)+")
     cve_patch_name_match = re.compile("(CVE\-\d{4}\-\d+)+")
     patched_cves = dict()
+    for key, value in cooker_data.file_checksums[realfn[0]].items():
+        patches = value.split()
+        for patch in patches:
+            patch_file, _, patch_data = patch.partition(':')
+            if patch_file.endswith('.patch') and patch_data == 'True':
+                with open(patch_file, "rb") as f:
+                    try:
+                        patch_text = f.read().decode('utf-8', 'replace')
+                    except UnicodeDecodeError:
+                        logger.info("Failed to read patch %s" % patch_file)
+                        f.close()
+                match = cve_match.search(patch_text)
+                if match:
+                    cves = patch_text[match.start()+5:match.end()]
+                    for cve in cves.split():
+                        try:
+                            if patch_file not in patched_cves[cve]:
+                                patched_cves[cve].append(patch_file)
+                        except KeyError:
+                            patched_cves[cve] = [patch_file]
+                else:
+                    match = cve_patch_name_match.search(patch_file)
+                    if match:
+                        cve = match.group(1)
+                        try:
+                            if patch_file not in patched_cves[cve]:
+                                patched_cves[cve].append(patch_file)
+                        except KeyError:
+                            patched_cves[cve] = [patch_file]
+    return patched_cves
 
-    # iterate over the recipes which would be built (pn-buildlist)
+
+def layer_dict(lyr):
+    # Keep a subset of all the layer info
+    return dict(remote=lyr['remote'], rev=lyr['revision'], branch=lyr['branch'])
+
+
+if __name__ == '__main__':
+    tf = setup_tinfoil(tracking=True)
+    distro = tf.config_data.get('DISTRO_CODENAME') or tf.config_data.get('DISTRO_NAME')
+    layer_info = {lyr['name']: layer_dict(lyr) for lyr in get_layer_info(tf.cooker)}
+
+    manifest = dict(date=datetime.utcnow().isoformat(),
+                    distro=distro,
+                    distro_version=tf.config_data.get('DISTRO_VERSION'),
+                    image=target,
+                    layers=layer_info,
+                    machine=tf.config_data.get('MACHINE'),
+                    packages=dict(),
+                    patched_cves=dict())
+
+    latest_versions, preferred_versions = bb.providers.findProviders(
+                                              tf.config_data,
+                                              tf.cooker_data,
+                                              tf.cooker_data.pkg_pn)
+
+    depgraph = tf.cooker.generatePkgDepTreeData([target], 'build')
     for p in depgraph['pn']:
         if p.endswith('-native'):
             continue
@@ -65,54 +118,26 @@ if cooker.start():
         pref = preferred_versions[p]
         realfn = bb.cache.virtualfn2realfn(pref[1])
         preffile = realfn[0]
-        # We only display once per recipe, we should prefer non extended versions of the
-        # recipe if present (so e.g. in OpenEmbedded, openssl rather than nativesdk-openssl
-        # which would otherwise sort first).
-        if realfn[1] and realfn[0] in cooker.recipecaches[''].pkg_fn:
+        # We only display once per recipe, we should prefer non extended
+        # versions of the recipe if present (so e.g. in OpenEmbedded, openssl
+        # rather than nativesdk-openssl which would otherwise sort first).
+        if realfn[1] and realfn[0] in tf.cooker_data.pkg_fn:
             continue
 
-        lyr = get_file_layer(cooker, preffile)
+        lyr = get_file_layer(tf.cooker, preffile)
         p_version = str("%s" % (pref[0][1]))
         info = layer_info.get(lyr)
-        branch = info['branch']
+        branch = info.get('branch', 'UNKNOWN')
+        cves = find_patched_cves(tf.cooker_data, realfn)
 
-        manifest['packages'][p] = dict(version=p_version, layer=lyr, branch=branch)
+        manifest['patched_cves'].update(cves)
+        manifest['packages'][p] = dict(version=p_version,
+                                       layer=lyr,
+                                       branch=branch)
 
-        for key,value in cooker.recipecaches[''].file_checksums[realfn[0]].items():
-            patches = value.split()
-            for patch in patches:
-                patch_file,_,patch_data = patch.partition(':')
-                if patch_file.endswith('.patch') and patch_data == 'True':
-                    with open(patch_file, "rb") as f:
-                        try:
-                            patch_text = f.read().decode('utf-8', 'replace')
-                        except UnicodeDecodeError:
-                            logger.info ("Failed to read patch %s" %  patch_file)
-                            f.close()
-                    match = cve_match.search(patch_text)
-                    if match:
-                        cves = patch_text[match.start()+5:match.end()]
-                        for cve in cves.split():
-                            try:
-                                if patch_file not in patched_cves[cve]:
-                                    patched_cves[cve].append(patch_file)
-                            except KeyError:
-                                patched_cves[cve] = [patch_file]
-                    else:
-                        match = cve_patch_name_match.search(patch_file)
-                        if match:
-                            cve = match.group(1)
-                            try:
-                                if patch_file not in patched_cves[cve]:
-                                    patched_cves[cve].append(patch_file)
-                            except KeyError:
-                                patched_cves[cve] = [patch_file]
-    manifest["patched_cves"] = patched_cves
-
-    s = json.dumps(manifest, indent=2)
+    s = json.dumps(manifest, indent=2, sort_keys=True)
     with open(ofile, "w") as f:
         f.write(s)
+
+    tf.shutdown()
     print('Done. Wrote manifest to "%s"' % ofile)
-    cooker.shutdown()
-else:
-    logger.error("Failed to start cooker. Exiting...")
