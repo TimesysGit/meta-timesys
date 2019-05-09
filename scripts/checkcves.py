11ab7498e6c8dd23ea42d18be274bfae8e29ec66
Patrick Mochel <patrick.mochel@timesys.com>
2019-05-08 17:00:16 -0700

    [vigiles] Add task-based Yocto CVE Scanner
    This implements a Yocto CVE scanner that runs automatically when an
    image is built if a user adds:
    
    	INHERIT += "vigiles"
    
    .. to local.conf.
    
    A manifest is generated in the build/vigiles/ directory, which is then
    sent to be compared against the Timesys CVE database. The response is
    written to a report the same directory.
    
    The source consists of the following:
    
    	classes/vigiles.bbclass	- Interface to the build process
    	classes/tsmeta.bbclass - Backend metadata collection
    	scripts/checkcves.py - Interface to remote CVE database
    	conf/vigiles.conf - Subsystem variables
    
    The algorithm uses a per-image task that relies on a per-package task
    that is executed when its packagedata is collected by the build process.
    This happens automatically when an image is built.
    
    Some more info can be found in the README.md.

--- scripts/checkcves.py
+++ scripts/checkcves.py
@@ -11,8 +11,9 @@ from lib import llapi, manifest
 
 NVD_BASE_URL = 'https://nvd.nist.gov/vuln/detail/'
 API_DOC = '%s/docs/wiki/engineering/LinuxLink_Key_File' % llapi.LINUXLINK_SERVER
-INFO_PAGE = 'https://www.timesys.com/open-source-embedded/security-update-management/'
+INFO_PAGE = 'https://www.timesys.com/security/vulnerability-patch-notification/'
 
+bogus_whitelist = "CVE-1234-1234"
 
 def get_usage():
     return('This script sends a json manifest file for an image to LinuxLink '
@@ -27,25 +28,25 @@ def get_usage():
 
 
 def print_demo_notice(bad_key=False):
-    if bad_key:
-        print('\nWarning: No API keyfile was found, or the contents were '
-              'invalid.\n\n'
-              'If you do have a LinuxLink subscription, please see this '
-              'document for API key information:\n'
-              '%s\n'
-              % API_DOC)
-    else:
-        print('\nWarning: No active subscription for this account.\n')
+    print('\n-- Vigiles Demo Mode Notice --', file=sys.stderr)
 
-    print('The script will continue in demo mode, which will link you to '
-          'temporarily available online results only.\n'
-          'You will need to login or register for a free account in order to '
-          'see the report.\n\n'
-          'You can not subscribe for recurring CVE notifications without an '
-          'active subscription!\n\n'
-          'For more information on the security notification service, visit:\n'
-          '%s\n'
-          % INFO_PAGE)
+    if bad_key:
+         print('\tNo API keyfile was found, or the contents were invalid.\n\n'
+              '\tPlease see this document for API key information:\n'
+              '\t%s\n' % API_DOC,
+              file=sys.stderr)
+    else:
+        print('\tNo active subscription for this account.\n', file=sys.stderr)
+
+    print('\tThe script will continue in demo mode, which will link you to '
+            'temporarily available online results only.\n'
+          '\tYou will need to login or register for a free account in order to '
+            'see the report.\n', 
+          file=sys.stderr)
+    print('\tFor more information on the security notification service, '
+            'please visit:\n'
+          '\t%s\n' % INFO_PAGE,
+          file=sys.stderr)
 
 
 def get_bb_root():
@@ -103,78 +104,39 @@ def read_manifest(manifest_file):
     return manifest_data
 
 
-def print_cves(result, demo=False, outfile=None):
-    cves = result['cves']
-    if demo:
-        print('\n-- CVE Summary --\n'
-              '\nUnfixed: %d\n'
-              'Unfixed, Patch Available: %d\n'
-              'Fixed: %d\n'
-              'CPU: %d'
-              % (cves['unfixed_count'],
-                 cves['unapplied_count'],
-                 cves['fixed_count'],
-                 cves['arch_count']),
-              file=outfile)
-        print('\n"CPU" CVEs are filed against the hardware, and may be fixed '
-              'or mitigated in other components such as the kernel or '
-              'compiler.\n'
-              '"Patch Available" CVEs have a fix available in the '
-              'meta-timesys-security layer. If the layer is already included, '
-              'then you may need to update your copy.',
-              file=outfile)
-        if outfile is not None:
-            print('\nDone. Results written to "%s"' %
-                  os.path.basename(outfile.name))
-        return
 
+
+def print_cves(result, demo=False, outfile=None):
     arch_cves = result.get('arch_cves', [])
     if arch_cves:
-        print('\n\n-- CPU / Architecture CVEs --\n'
-              '\nNote: These are CVEs which are filed against the hardware, '
-              'and may be fixed or mitigated in other components such as the '
-              'kernel or compiler.',
-              file=outfile)
+        print('\n\n-- Architecture CVEs --', file=outfile)
         for cve in arch_cves:
-            print('\nCVE ID:  %s' % cve['cve_id'], file=outfile)
-            print('URL:     %s%s' % (NVD_BASE_URL, cve['cve_id']), file=outfile)
-            print('CVSSv2:  %s' % cve['cvss'], file=outfile)
-            print('Vector:  %s' % cve['vector'], file=outfile)
+            print('\n\tCVE ID:  %s' % cve['cve_id'], file=outfile)
+            print('\tURL:     %s%s' % (NVD_BASE_URL, cve['cve_id']), file=outfile)
+            print('\tCVSSv3:  %s' % cve['cvss'], file=outfile)
+            print('\tVector:  %s' % cve['vector'], file=outfile)
 
+    cves = result.get('cves', {})
     if cves:
         print('\n\n-- Recipe CVEs --', file=outfile)
         for pkg, info in cves.items():
             for cve in info:
-                print('\nRecipe:  %s' % pkg, file=outfile)
-                print('Version: %s' % cve['version'], file=outfile)
-                print('CVE ID:  %s' % cve['cve_id'], file=outfile)
-                print('URL:     %s%s' % (NVD_BASE_URL, cve['cve_id']), file=outfile)
-                print('CVSSv2:  %s' % cve['cvss'], file=outfile)
-                print('Vector:  %s' % cve['vector'], file=outfile)
-                print('Status:  %s' % cve['status'], file=outfile)
+                print('\n\tRecipe:  %s' % pkg, file=outfile)
+                print('\tVersion: %s' % cve['version'], file=outfile)
+                print('\tCVE ID:  %s' % cve['cve_id'], file=outfile)
+                print('\tURL:     %s%s' % (NVD_BASE_URL, cve['cve_id']), file=outfile)
+                print('\tCVSSv2:  %s' % cve['cvss'], file=outfile)
+                print('\tVector:  %s' % cve['vector'], file=outfile)
+                print('\tStatus:  %s' % cve['status'], file=outfile)
                 patches = cve.get('fixedby')
                 if patches:
                     if cve['status'] == 'Unfixed, Patch Available':
-                        print('Patched in meta-timesys-security commit(s):',
+                        print('\tPatched in meta-timesys-security commit(s):',
                               file=outfile)
                     else:
-                        print('Patched by:', file=outfile)
+                        print('\tPatched by:', file=outfile)
                     for patch in patches:
-                        print('\t%s' % patch, file=outfile)
-    if outfile is not None:
-        print('\nDone. Results written to "%s"' %
-              os.path.basename(outfile.name))
-
-
-def print_url(result, demo=False, outfile=None):
-    report_url = '%s%s' % (llapi.LINUXLINK_SERVER, result['report_path'])
-    # print summary to both stdout and output file
-    if outfile is not None:
-        print_url(result, demo=demo, outfile=None)
-    print('\nView the complete report online at:\n%s\n' % report_url,
-          file=outfile)
-    if demo:
-        print('Note: The above URL will expire after one day.', file=outfile)
+                        print('\t* %s' % patch, file=outfile)
 
 
 def parse_cve_counts(counts, category):
@@ -196,60 +158,99 @@ def parse_cvss_counts(counts, severity):
 
 
 def print_summary(result, outfile=None):
-    # print summary to both stdout and output file
-    if outfile is not None:
-        print_summary(result, None)
-
-    counts = result.get('counts', {})
-    unfixed = parse_cve_counts(counts, 'unfixed')
-    unapplied = parse_cve_counts(counts, 'unapplied')
-    fixed = parse_cve_counts(counts, 'fixed')
-
-    cvss_counts = counts.get('cvss_counts', {})
-    cvss_total = parse_cvss_counts(cvss_counts, 'high')
-    cvss_kernel = parse_cvss_counts(cvss_counts.get('kernel', {}), 'high')
-    cvss_toolchain = parse_cvss_counts(cvss_counts.get('toolchain', {}), 'high')
-    cvss_rfs = cvss_total - cvss_kernel - cvss_toolchain
-
-    print('\n\n-- Summary --', file=outfile)
-    print('\nUnfixed: {} ({} RFS, {} Kernel, {} Toolchain)'.format(
-            unfixed['total'], unfixed['rfs'],
-            unfixed['kernel'], unfixed['toolchain']),
-          file=outfile)
-    print('Unfixed, Patch Available: '
-          '{} ({} RFS, {} Kernel, {} Toolchain)'.format(
-            unapplied['total'], unapplied['rfs'],
-            unapplied['kernel'], unapplied['toolchain']),
-          file=outfile)
-    print('Fixed: {} ({} RFS, {} Kernel, {} Toolchain)'.format(
-            fixed['total'], fixed['rfs'], fixed['kernel'], fixed['toolchain']),
-          file=outfile)
-    print('High CVSS: {} ({} RFS, {} Kernel, {} Toolchain)'.format(
-            cvss_total, cvss_rfs, cvss_kernel, cvss_toolchain),
-          file=outfile)
 
 
-def print_whitelist(manifest, outfile=None):
-    wl = manifest.get('whitelist')
-    print('\n\n-- Whitelist --', file=outfile)
-    print('\nThese recipes or CVE IDs are listed in the "CHECKCVES_WHITELIST" '
-          'variable and\nare NOT included in the report:\n',
-          file=outfile)
+    def show_header(f_out=outfile):
+      print('\n-- Vigiles CVE Report --', file=f_out)
+
+      report_url = '%s%s' % (llapi.LINUXLINK_SERVER, result['report_path'])
+
+      print('\n\tView detailed online report at:\n'
+              '\t  %s' % report_url, file=f_out)
+
+      if (demo):
+        print('\t  NOTE: Running in Demo Mode will cause this URL to expire '
+          'after one day.', file=f_out)
+
+    def show_subscribed_summary(f_out=outfile):
+      counts = result.get('counts', {})
+      unfixed = parse_cve_counts(counts, 'unfixed')
+      unapplied = parse_cve_counts(counts, 'unapplied')
+      fixed = parse_cve_counts(counts, 'fixed')
+
+      cvss_counts = counts.get('cvss_counts', {})
+      cvss_total = parse_cvss_counts(cvss_counts, 'high')
+      cvss_kernel = parse_cvss_counts(cvss_counts.get('kernel', {}), 'high')
+      cvss_toolchain = parse_cvss_counts(cvss_counts.get('toolchain', {}), 'high')
+      cvss_rfs = cvss_total - cvss_kernel - cvss_toolchain
+
+      print('\n\tUnfixed: {} ({} RFS, {} Kernel, {} Toolchain)'.format(
+              unfixed['total'], unfixed['rfs'],
+              unfixed['kernel'], unfixed['toolchain']),
+            file=f_out)
+      print('\tUnfixed, Patch Available: '
+            '{} ({} RFS, {} Kernel, {} Toolchain)'.format(
+              unapplied['total'], unapplied['rfs'],
+              unapplied['kernel'], unapplied['toolchain']),
+            file=f_out)
+      print('\tFixed: {} ({} RFS, {} Kernel, {} Toolchain)'.format(
+              fixed['total'], fixed['rfs'], fixed['kernel'], fixed['toolchain']),
+            file=f_out)
+      print('\tHigh CVSS: {} ({} RFS, {} Kernel, {} Toolchain)'.format(
+              cvss_total, cvss_rfs, cvss_kernel, cvss_toolchain),
+            file=f_out)
+
+    def show_demo_summary(f_out=outfile):
+      print('\n-- Vigiles CVE Overview --', file=f_out)
+      print('\n\tUnfixed: %d\n'
+      '\tUnfixed, Patch Available: %d\n'
+      '\tFixed: %d\n'
+      '\tCPU: %d'
+      % (cves['unfixed_count'],
+         cves['unapplied_count'],
+         cves['fixed_count'],
+         cves['arch_count'],),
+            file=f_out)
+
+    show_header(outfile)
+
+    if not demo:
+      show_subscribed_summary(outfile)
+    else:
+      show_demo_summary(outfile)
+
+def print_foootnotes(f_out=None):
+    print('\n-- Vigiles Footnotes --', file=f_out)
+    print('\t* "CPU" CVEs are filed against the hardware.\n'
+          '\t  They may be fixed or mitigated in other components such as '
+                'the kernel or compiler.\n',
+          file=f_out)
+
+    print('\t* "Patch Available" CVEs have a fix available in the '
+                'meta-timesys-security layer.\n'
+          '\t  If the layer is already included, then you may need to '
+                'update your copy.\n',
+          file=f_out)
+
+    print('\t* "Whitelist" Recipes and CVEs are listed in the '
+                '"VIGILES_WHITELIST" variable.\n'
+          '\t  They are NOT included in the report.\n',
+      file=f_out)
+
+
+def print_whitelist(wl, outfile=None):
+    print('\n-- Vigiles CVE Whitelist --\n', file=outfile)
     if wl:
         for item in sorted(wl):
-            # the default list contains a harmless but bogus example CVE ID,
-            # don't print it here in case that is confusing.
-            if item == 'CVE-1234-1234':
-                continue
-            print('* %s' % item, file=outfile)
+            print('\t* %s' % item, file=outfile)
     else:
-            print('(Nothing is Whitelisted)', file=outfile)
+            print('\t(Nothing is Whitelisted)', file=outfile)
 
 
 if __name__ == '__main__':
     resource = '/api/cves/reports/yocto/'
     home_dir = os.path.expanduser('~')
-    key_file = os.getenv('KEY_FILE', '%s/timesys/linuxlink_key' % home_dir)
+    key_file = os.getenv('VIGILES_KEY_FILE', '%s/timesys/linuxlink_key' % home_dir)
     demo = False
     args = handle_cmdline_args()
 
@@ -321,6 +322,8 @@ if __name__ == '__main__':
         manifest_data = generate_manifest(im)
         im.shutdown()
 
+    manifest = json.loads(manifest_data)
+
     # If -k is specified, the given config file is submitted along with the
     # manifest to filter out irrelevant kernel CVEs
     if not args.kconfig:
@@ -332,15 +335,22 @@ if __name__ == '__main__':
         except (OSError, IOError, UnicodeDecodeError) as e:
             print('Error: Could not open kernel config: %s' % e)
             sys.exit(1)
+        print('Vigiles: Kernel Config based filtering has been applied', file=sys.stderr)
 
-    print('\nRequesting image analysis from LinuxLink ...\n')
+    print('Vigiles: Requesting image analysis from LinuxLink ...\n', file=sys.stderr)
     result = llapi.api_post(email, key, resource,
                             {'manifest': manifest_data,
                              'kconfig': kernel_config,
                              'subscribe': args.subscribe})
-    cves = result.get('cves', [])
-    arch_cves = result.get('arch_cves', [])
+    # the default list contains a harmless but bogus example CVE ID,
+    # don't print it here in case that is confusing.
+    whitelist = [ item for item in manifest.get('whitelist', []) 
+      if not any(bogon == item for bogon in bogus_whitelist.split()) ]
 
+    print('-- Vigiles CVE Scanner --\n\n'
+            '\t%s\n\n' % INFO_PAGE, file=outfile)
+    print('-- Date Generated (UTC) --\n', file=outfile)
+    print('\t%s' % result['date'], file=outfile)
 
     # If no LinuxLink subscription or bogus user/key, it will have fallen back
     # to demo mode
@@ -353,26 +363,33 @@ if __name__ == '__main__':
     # account / seat:
     sub_result = result.get('subscribed', False)
     if args.subscribe:
+        print('\n-- Vigiles CVE Weekly Report --\n', file=outfile)
         if not sub_result:
-            print('Warning: Could not subscribe to weekly CVE report!\n'
-                  'Please check that you have an active LinuxLink '
-                  'subscription.\n')
+            print('\tWarning: Could not subscribe to weekly CVE report!\n'
+                '\t  Please check that you have an active LinuxLink '
+                'subscription.\n', file=outfile)
         else:
-            print('Notice: You subscribed to weekly email notifications for '
-                  'this report.\nMake sure that you are allowing update emails '
-                  'in your LinuxLink preferences.\n')
+            print('\tNotice: You subscribed to weekly email notifications for '
+                  'this report.\n'
+                  '\tMake sure that you are allowing update emails in your '
+                  'LinuxLink preferences.\n', file=outfile)
+
+
+    cves = result.get('cves', [])
+    arch_cves = result.get('arch_cves', [])
 
     if not cves and not arch_cves:
-        print('No results.')
+        print('Vigiles: No results.. Exiting.')
         sys.exit(0)
 
-    print('-- Date Generated (UTC) --\n', file=outfile)
-    print('%s' % result['date'], file=outfile)
+    print_summary(result, outfile=outfile)
 
-    print_cves(result, demo=demo, outfile=outfile)
-    print_whitelist(manifest=json.loads(manifest_data), outfile=outfile)
     if not demo:
-        print_summary(result, outfile=outfile)
-        if kernel_config:
-            print('Note: Kernel Config based filtering has been applied')
-    print_url(result, demo=demo, outfile=outfile)
+      print_cves(result, demo=demo, outfile=outfile)
+    print_whitelist(whitelist, outfile=outfile)
+    print_foootnotes(f_out=outfile)
+
+    if outfile is not None:
+      print_summary(result)
+      print('\n\tLocal summary written to:\n\t  %s' %
+            os.path.relpath(outfile.name))
