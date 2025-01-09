TimesysGit/meta-timesys

commit 85d1ac7d18af89e3168f83519b0428f6321a8d6e

[general] update get_recipe_pv_without_srcpv to get_recipe_pv_with_pfx_sfx
    With commit https://github.com/yoctoproject/poky/commit/d3e4f25993cc784efc61c69a9c25daf5f89102bb function get_recipe_pv_without_srcpv
    has been changed to get_recipe_pv_with_pfx_sfx

classes/tsmeta.bbclass
@@ -328,7 +328,7 @@ def _get_cve_version(d):
     if not cve_v:
         pv = d.getVar('PV')
         uri_type = 'git' if ('git' in pv or 'AUTOINC' in pv) else ''
-        (bpv, pfx, sfx) = oe.get_recipe_pv_without_srcpv(pv, uri_type)
+        (bpv, pfx, sfx) = oe.get_recipe_pv_with_pfx_sfx(pv, uri_type)
         cve_v = bpv
     return cve_v
 

classes/vigiles.bbclass
@@ -769,7 +769,7 @@ def _get_uboot_pf(d):
     pv = tsmeta_read_dictname_single(d, 'pn', boot_pn, 'pv')
     if not pv:
         pv = 'unset'
-    (bpv, pfx, sfx) = oe.get_recipe_pv_without_srcpv(pv, 'git')
+    (bpv, pfx, sfx) = oe.get_recipe_pv_with_pfx_sfx(pv, 'git')
 
     vgls_pf = '-'.join([boot_pn, bpv])
     return vgls_pf

commit f7b992038dcfc50d48c589268330c11c91703f7c

[general] fix kirkstone support
For yocto kirkstone we need to use oe.get_recipe_pv_without_srcpv()
instead of oe.get_recipe_pv_with_pfx_sfx() because
oe.get_recipe_pv_with_pfx_sfx() doesn't exist for kirkstone.

Let's determine the right function name to call at runtime.

This fixes an issue introduced by commit
85d1ac7d18af89e3168f83519b0428f6321a8d6e.

Signed-off-by: Viacheslav Volkov <convivo@nibeconsultants.eu>
Signed-off-by: Måns Andersson <mans@nibejpi.com>

classes/tsmeta.bbclass
@@ -309,9 +309,16 @@ def _detect_uboot_version(d):
             _version += _extra
     return _version
 
-def _get_cve_version(d):
+def _get_recipe_pv_with_pfx_sfx(pv, uri_type):
     import oe.recipeutils as oe
+    if hasattr(oe, 'get_recipe_pv_with_pfx_sfx'):
+        func = oe.get_recipe_pv_with_pfx_sfx
+    else:
+        # For yocto kirkstone or older:
+        func = oe.get_recipe_pv_without_srcpv
+    return func(pv, uri_type)
 
+def _get_cve_version(d):
     cve_v = d.getVar('CVE_VERSION')
     if bb.data.inherits_class('kernel', d):
         cve_v = _detect_kernel_version(d)
@@ -328,7 +335,7 @@ def _get_cve_version(d):
     if not cve_v:
         pv = d.getVar('PV')
         uri_type = 'git' if ('git' in pv or 'AUTOINC' in pv) else ''
-        (bpv, pfx, sfx) = oe.get_recipe_pv_with_pfx_sfx(pv, uri_type)
+        (bpv, pfx, sfx) = _get_recipe_pv_with_pfx_sfx(pv, uri_type)
         cve_v = bpv
     return cve_v
 

classes/vigiles.bbclass
@@ -848,8 +848,6 @@ do_vigiles_kconfig[nostamp] = "1"
 
 
 def _get_uboot_pf(d):
-    from oe import recipeutils as oe
-
     pn = d.getVar('PN')
     boot_pn = d.getVar('VIGILES_UBOOT_PN') or \
         d.getVar('PREFERRED_PROVIDER_virtual/bootloader') or ''
@@ -860,7 +858,7 @@ def _get_uboot_pf(d):
     pv = tsmeta_read_dictname_single(d, 'pn', boot_pn, 'pv')
     if not pv:
         pv = 'unset'
-    (bpv, pfx, sfx) = oe.get_recipe_pv_with_pfx_sfx(pv, 'git')
+    (bpv, pfx, sfx) = _get_recipe_pv_with_pfx_sfx(pv, 'git')
 
     vgls_pf = '-'.join([boot_pn, bpv])
     return vgls_pf